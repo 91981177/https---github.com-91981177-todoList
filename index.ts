d11e5b00c56623fa00ad5522fdd0bd4586973b73
91981177 <muhammadishaquedanish@gmail.com>
2023-01-24 08:08:36 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+
+
+let todos:string[]=[]
+let loop=true
+while(true){
+    const answer:{
+        TODO:string,
+        addmore:boolean
+    }=await inquirer.prompt([
+        {
+            type:"input",
+            name:"TODO",
+            message:"What Do You want to add in your todo?"
+        },
+        {
+            type:"confirm",
+            name:"addmore",
+            message:"Do You want to add more in your todo?",
+            default:false
+        }
+    ])
+    const{TODO,addmore}=answer
+    console.log(answer)
+    loop=addmore
+    if(TODO){
+todos.push(TODO)
+    }else{
+        console.log("kindly add valid input")
+    }
+}
+ if(todos.length > 0){
+    console.log("your todo list!");
+    
+    todos.forEach( todo=> {
+        console.log(todo)
+    });
+ }else{
+    console.log("not founf todo!!");
+    
+ }
